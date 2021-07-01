7de0bd08e82d7fe455a8ba7b5a6cd5239f41a48f
yash0112 <85066851+yash0112@users.noreply.github.com>
2021-07-02 00:21:34 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<cs50.h>
+
+void swap_max(int a[], int l, int p){
+
+ int max=a[p-1],t;
+ for(int i=p-1; i<l; i++){
+  if(max<a[i])
+    max=a[i];
+ }
+ printf("%d\n", max);
+ printf("%d\n", a[p-1]);
+ for(int i=0; i<l; i++){
+  if(max==a[i]){
+    t=a[p-1];
+    a[p-1]=max;
+    a[i]=t;
+    }
+   }
+   }
+
+
+void ssort(int a[], int l){
+ for(int i=0; i<l; i++){
+   swap_max(a, l, i);
+  }
+ }
+ 
+   
